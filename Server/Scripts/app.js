9aca4f9458cb5f0d60be58038fb94e7c1106c017
Saahil Claypool <saahil@claypools.org>
2024-01-27 05:27:12 +0000

    morph update

--- Server/Scripts/app.js
+++ Server/Scripts/app.js
@@ -1,5 +1,6 @@
 import Alpine from 'alpinejs'
-import './htmx.js'
+import * as htmxEsm from 'htmx.org/dist/htmx.esm'
+import * as idiomorphEsm from 'idiomorph/dist/idiomorph.esm'
 
 window.Alpine = Alpine;
 
@@ -8,3 +9,25 @@ Alpine.start()
 window.sleep = function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+function createMorphConfig(swapStyle) {
+    if (swapStyle === 'morph' || swapStyle === 'morph:outerHTML') {
+        return { morphStyle: 'outerHTML' }
+    } else if (swapStyle === 'morph:innerHTML') {
+        return { morphStyle: 'innerHTML' }
+    } else if (swapStyle.startsWith("morph:")) {
+        return Function("return (" + swapStyle.slice(6) + ")")();
+    }
+}
+
+htmxEsm.htmx.defineExtension('morph', {
+    isInlineSwap: function (swapStyle) {
+        let config = createMorphConfig(swapStyle);
+        return config.swapStyle === "outerHTML" || config.swapStyle == null;
+    },
+    handleSwap: function (swapStyle, target, fragment) {
+        let config = createMorphConfig(swapStyle);
+        if (config) {
+            return idiomorphEsm.Idiomorph.morph(target, fragment.children, config);
+        }
+    }
+});
